81eee20340102f2bffef4cfaac458b9982096b82
Charles Winikates <cwinika@cs.okstate.edu>
2025-05-01 16:32:17 -0500

    first draft

---
+++ monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true; do
+clear
+echo "--------------------SYSTEM--------------------"
+echo "Time: $(date)"
+echo "Uptime: $(uptime -p)"
+echo
+echo "CPU:"
+mpstat 1 1 | grep "Average"
+echo
+echo "Memory:"
+free -h
+echo
+echo "Disk:"
+df -h /
+echo
+echo "Top 5 CPU intensive processes:"
+ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
+echo
+sleep 5
+done
