9177cd0afbd784fbf385b4a4b6dafd7199817a35
Winchar124 <charliejohnw@icloud.com>
2025-05-06 20:08:50 -0500

    expanded functionality push 4 5/6/2025

--- monitor.sh
+++ monitor.sh
@@ -7,17 +7,21 @@ echo
 echo "Time: $(date)"
 echo "Uptime: $(uptime -p)"
 echo
-echo "CPU:"
-mpstat 1 1 | grep "Average"
-echo
 echo "Memory:"
 free -h
 echo
 echo "Disk:"
 df -h /
 echo
+echo "CPU:"
+mpstat 1 1 | grep "Average"
 echo "Top 5 CPU intensive processes:"
 ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
 echo
+echo "Active Users:"
+who
+echo
+echo "Network:"
+netstat -i
 sleep 5
 done
